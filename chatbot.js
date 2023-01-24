5bcfcfc173727b618d1b4bdd12f2cfee07b006e1
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2023-01-24 21:43:39 +0200

    chatbot fix

--- chatbot.js
+++ chatbot.js
@@ -1,6 +1,12 @@
 addEventListener("DOMContentLoaded", function(){
-    document.getElementById("chatbot-input").addEventListener("keypress", keyPressed);
-    document.getElementById("chatbot-btn").addEventListener("click", sendMessage);
+    const input = document.getElementById("chatbot-input");
+    const btn = document.getElementById("chatbot-btn");
+    const messages = document.getElementById("chatbot-messages");
+    const spinner = document.getElementById("chatbot-spinner");
+    const container = document.getElementById("chatbot-massages-scroll");
+
+    input.addEventListener("keypress", keyPressed);
+    btn.addEventListener("click", sendMessage);
     
     function keyPressed(event){
         if (event.key == "Enter") {
@@ -9,24 +15,30 @@ addEventListener("DOMContentLoaded", function(){
     }
 
     function sendMessage(){
-        var text = document.getElementById("chatbot-input").value;
+        var text = input.value;
         if (text){
-            document.getElementById("chatbot-messages").innerHTML += `<p class="out-message">${text}</p>`;
-            document.getElementById("chatbot-input").value = '';
+            messages.innerHTML += `<p class="out-message">${text}</p>`;
+            input.value = '';
             chatReply(text);
         }
     }
 
     function chatReply(text){
         var proto = `H&M може обробляти персональні дані, зібрані під час співбесіди чи опитування, щоб покращити якість послуг, що надаються. Детальну інформацію можна знайти в нашій Політиці конфіденційності`;
-        document.getElementById("chatbot-spinner").style.display = "block";
+        spinner.style.display = "block";
+        scrollDown();
         setTimeout(
             function(){
                 var reply = proto.split(" ").sort(() => Math.random() - 0.5).slice(0, 5 + Math.round(45 * Math.random())).join(" ");
-                document.getElementById("chatbot-messages").innerHTML += `<p class="in-message">${reply}</p>`;
-                document.getElementById("chatbot-spinner").style.display = "none";
+                messages.innerHTML += `<p class="in-message">${reply}</p>`;
+                spinner.style.display = "none";
+                scrollDown();
             }, 
             1000 + 1000*Math.random()
         );
+    }
+
+    function scrollDown(){
+        container.scrollTop = messages.clientHeight;
     }
 })
